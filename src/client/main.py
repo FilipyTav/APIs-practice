2235904538957b40ff54fd1d553efbb34cffd40c
Filipy <filipytav17@gmail.com>
2026-05-17 15:09:50 -0300

    Better client display

--- src/client/main.py
+++ src/client/main.py
@@ -1,17 +1,18 @@
 import json
+from typing import Any
 import requests
 
 BASE_URL: str = "http://127.0.0.1:8000"
 game_name: str = "elden ring"
 
 
-def fetch_game(name: str) -> str:
+def fetch_game(name: str) -> dict[Any, str]:
     try:
         response: requests.Response = requests.get(f"{BASE_URL}/games/{name}")
 
         if response.status_code == 200:
-            data: str = response.json()
-            return json.dumps(data, indent=4)
+            data: dict[Any, str] = response.json()
+            return data
         else:
             print(f"Failed with status code: {response.status_code}")
             print(f"Details: {response.text}")
@@ -19,11 +20,11 @@ def fetch_game(name: str) -> str:
     except:
         print("Error: Could not get response from server.")
 
-    return ""
+    return {}
 
 
-game_data: str = fetch_game(game_name)
+game_data: dict[Any, str] = fetch_game(game_name)
 if game_data:
     print("------------------------")
-    print(game_data)
+    print(json.dumps(game_data, indent=4))
     print("------------------------")
